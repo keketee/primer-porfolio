11ee9ebfe02046d391df9633e8aec3879b88f2ef
keketee <elgrankeko@hotmail.com>
2024-05-07 19:09:40 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const buttonMenuElement = document.getElementById('button__menu');
+const menuElement = document.getElementById('menu');
+
+function handleMenu() {
+  if (menuElement.classList.contains('show')) {
+    menuElement.classList.remove('show');
+  } else {
+    menuElement.classList.add('show');
+  }
+}
+
+buttonMenuElement.addEventListener('click', handleMenu);
